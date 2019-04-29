65ebaf31a51309086dfc113be179a540ca116688
Gant <GantMan@gmail.com>
2019-04-29 00:01:33 -0500

    fixups and ignores

--- keras_to_tf.sh
+++ keras_to_tf.sh
@@ -8,12 +8,11 @@ fi
 
 # No name passed? Just use "model"
 NAME=${2:-model}
-RESULT_DIR="TF_${NAME}"
+RESULT_DIR="results/TF_${NAME}"
 K2TF="/Users/gantman/Documents/Projects/ml/keras_to_tensorflow/keras_to_tensorflow.py"
 
-mkdir $RESULT_DIR
 # convert to Tensorflow (expects python3 and keras_to_tensorflow)
 # https://github.com/amir-abdi/keras_to_tensorflow
 python3 $K2TF --input_model=$1 --output_model=./$RESULT_DIR/$NAME.pb
 # Quantized version
-python3 $K2TF quantize=true --input_model=$1 --output_model=./$RESULT_DIR/quant_$NAME.pb
+python3 $K2TF --quantize=true --input_model=$1 --output_model=./$RESULT_DIR/quant_$NAME.pb
--- keras_to_tfjs.sh
+++ keras_to_tfjs.sh
@@ -8,13 +8,12 @@ fi
 
 # No name passed? Just use "model"
 NAME=${2:-model}
-RESULT_DIR="TFJS_${NAME}"
+RESULT_DIR="results/TFJS_${NAME}"
 
-mkdir $RESULT_DIR
 # Regular
 tensorflowjs_converter --input_format keras $1 ./$RESULT_DIR/tfjs_$NAME
 zip ./$RESULT_DIR/tfjs_$NAME.zip ./$RESULT_DIR/tfjs_$NAME/
 # Quantized
 tensorflowjs_converter --quantization_bytes 1 --input_format keras $1 ./$RESULT_DIR/tfjs_quant_$NAME
-zip ./$RESULT_DIR/tfjs_min_$NAME_min.zip ./$RESULT_DIR/tfjs_quant_$NAME/
+zip ./$RESULT_DIR/tfjs_quant_$NAME.zip ./$RESULT_DIR/tfjs_quant_$NAME/
 
